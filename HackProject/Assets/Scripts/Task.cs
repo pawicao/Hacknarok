276f1710ddb97102d68f7df15c4c03d61cdad757
Oskar <pawicao@gmail.com>
2019-04-06 18:00:26 +0200

    taski4

--- HackProject/Assets/Scripts/Task.cs
+++ HackProject/Assets/Scripts/Task.cs
@@ -8,12 +8,13 @@ public class Task : MonoBehaviour
     private float timeLimit = 30.0f;
     private Image timeIndicator;
     private float timeLeft;
-    public static void CreateTask(GameObject taskPrefab)
+    public static Task CreateTask(GameObject taskPrefab, string taskName)
     {
         GameObject newTask = Instantiate(taskPrefab, TaskManager.instance.transform);
-        newTask.GetComponentInChildren<Text>().text = "Hehehue";
+        newTask.GetComponentInChildren<Text>().text = taskName;
+        return newTask.GetComponent<Task>();
     }
-   
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +27,16 @@ public class Task : MonoBehaviour
     {
         timeLeft -= Time.deltaTime;
         timeIndicator.fillAmount = timeLeft/timeLimit;
+        if (timeLeft <= 0)
+        {
+            Perish(false);
+        }
     }
-    
-    
+
+    private void Perish(bool haveSucceeded)
+    {
+        if(haveSucceeded)
+            
+        Destroy(gameObject);
+    }
 }
--- HackProject/Assets/Scripts/TaskManager.cs
+++ HackProject/Assets/Scripts/TaskManager.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskManager : MonoBehaviour
 {
     public float overallTimeLimit;
+    private float timeLeft;
     public GameObject taskPrefab;
 
     public static TaskManager instance;
+
+    private int minutesLeft;
+    private int secondsLeft;
+
+    private Text timeLeftText;
+
+    private List<Task> taskList = new List<Task>();
     // Start is called before the first frame update
     void Start()
     {
+        timeLeft = overallTimeLimit;
+        timeLeftText = GetComponentInChildren<Text>();
     }
 
     private void Awake() {
@@ -20,10 +31,24 @@ public class TaskManager : MonoBehaviour
             Destroy(gameObject);
         
     }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
-            Task.CreateTask(taskPrefab);
+        {
+            taskList.Add(Task.CreateTask(taskPrefab, "hehe"));
+        }
+
+        timeLeft -= Time.deltaTime;
+        secondsLeft = (int) timeLeft % 60;
+        minutesLeft = (int) timeLeft / 60;
+        if(secondsLeft < 10)
+            timeLeftText.text = "0" + minutesLeft + ":0" + secondsLeft;
+        else
+            timeLeftText.text = "0" + minutesLeft + ":" + secondsLeft;
+        
+        if(timeLeft <= 0 )
+            Application.Quit();
     }
 }
