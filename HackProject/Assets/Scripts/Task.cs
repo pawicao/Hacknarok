aa33c64002353c8ce6e4076286200e44ae151920
Oskar <pawicao@gmail.com>
2019-04-06 16:17:09 +0200

    taski2

--- HackProject/Assets/Scripts/Task.cs
+++ HackProject/Assets/Scripts/Task.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Task : MonoBehaviour
 {
-    public float timeLimit;
-    public new string name;
+    private float timeLimit;
+    private Image timeIndicator;
+
+    private float initTime;
+
+    private void SetNew(string name, float timeLimit)
+    {
+        this.timeLimit = timeLimit;
+        initTime = Time.time;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        timeIndicator = GetComponentInChildren<Image>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float timePassed = Time.time - initTime;
+        timeIndicator.fillAmount = timePassed / initTime;
     }
+    
+    
 }
--- HackProject/Assets/Scripts/TaskManager.cs
+++ HackProject/Assets/Scripts/TaskManager.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class TaskManager : MonoBehaviour
 {
     public float overallTimeLimit;
-
+    public GameObject taskPrefab;
     private List<Task> tasksList;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void AddTask()
+    {
+        //tasksList.Add(Instantiate());
+        tasksList.Add(Instantiate(taskPrefab, transform));
     }
 }
