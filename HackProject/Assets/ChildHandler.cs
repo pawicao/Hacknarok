799d4c8a6280e3f90a4a3a9753311c07cf12aa67
konrad <konrad@polarczyk.com>
2019-04-07 00:19:47 +0200

    Child drop

--- HackProject/Assets/ChildHandler.cs
+++ HackProject/Assets/ChildHandler.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChildHandler : MonoBehaviour
-{
+public class ChildHandler : MonoBehaviour {
+    public bool isEquipped = false;
+    public Child _child;
     public void Equip(Child child) {
-        child.transform.parent = transform;
-        child.transform.localPosition = -Vector3.forward;
+        _child = child;
+        _child.transform.parent = transform;
+        _child.transform.localPosition = -Vector3.forward;
+        isEquipped = true;
+    }
+
+    public void Remove() {
+        _child.transform.parent = null;
+        _child = null;
+        isEquipped = false;
+    }
+
+    public void Destroy() {
+        Destroy(_child);
+        Remove();
     }
 }
