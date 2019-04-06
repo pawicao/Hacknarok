7043395d28061fd0cd7e2bb9336ce75f20de4f7e
Oskar <pawicao@gmail.com>
2019-04-06 14:59:32 +0200

    item itd

---
+++ HackProject/Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<Item> itemList;
+
+    private void Add(Item item)
+    {
+        itemList.Add(item);
+    }
+
+    private void Remove(Item item)
+    {
+        foreach (Item i in itemList)
+        {
+            if (i.GetType() == item.GetType())
+            {
+                itemList.Remove(i);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
