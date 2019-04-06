4ddc049fae4c54ea9f0ef337b05cf51d163c376c
konrad <konrad@polarczyk.com>
2019-04-06 23:51:09 +0200

    Child + interact button

---
+++ HackProject/Assets/Child.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Child : MonoBehaviour, Interactable
+{
+    public void Interact(InteractController controller) {
+        ChildHandler childHandler = controller.GetComponentInChildren<ChildHandler>();
+        childHandler.Equip(this);
+    }
+}
