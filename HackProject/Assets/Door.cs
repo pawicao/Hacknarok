393fcb52482219a905d9b2e7c3d0629a2d92548c
konrad <konrad@polarczyk.com>
2019-04-06 20:42:15 +0200

    Door1

---
+++ HackProject/Assets/Door.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, Interactable {
+    public enum Axis {
+        X,
+        Y
+    };
+
+    public Axis wingAxis;
+    
+    private bool _isClosed;
+    private bool isClosed {
+        get { return _isClosed; }
+        set {
+            _isClosed = value;
+            foreach (Transform child in transform) {
+                child.gameObject.SetActive(value);
+            }
+        }
+    }
+    
+    public void Interact(InteractController controller) {
+        isClosed = false;
+    }
+
+    private void Update() {
+        if (isClosed) {
+            
+        }
+    }
+
+    private Vector2 GetWingVector() {
+        switch (wingAxis) {
+            case Axis.X:
+                return Vector2.up;
+            case Axis.Y:
+                return Vector2.right;
+            default:
+                throw new Exception("Wrong axis");
+        }
+    }
+}
