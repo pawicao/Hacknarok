bdeb5802b7bed3e57757ffee52565969702dc1a6
konrad <konrad@polarczyk.com>
2019-04-07 02:30:00 +0200

    Child finally

--- HackProject/Assets/Child.cs
+++ HackProject/Assets/Child.cs
@@ -6,6 +6,12 @@ public class Child : MonoBehaviour, Interactable
 {
     public void Interact(InteractController controller) {
         ChildHandler childHandler = controller.GetComponentInChildren<ChildHandler>();
+
+        ChildHandler currentHandler = GetComponentInParent<ChildHandler>();
+        if (currentHandler && currentHandler.isEquipped && currentHandler != childHandler) {
+            currentHandler.Remove();
+        }
+        
         if (childHandler.isEquipped) {
             childHandler.Remove();
             return;
